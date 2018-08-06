2295607707354adae4e58ab83924d09c79074387
Ulugbek Aripov <ulugbeks@gmail.com>
2018-08-06 19:35:25 +0500

    Companions list basic setup is done

--- src/app/companions-list/companion-item/companion-item.component.ts
+++ src/app/companions-list/companion-item/companion-item.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-companion-item',
   templateUrl: './companion-item.component.html',
   styleUrls: ['./companion-item.component.css']
 })
-export class CompanionItemComponent implements OnInit {
 
+export class CompanionItemComponent implements OnInit {
+  @Input('companion') companion: any;
+  selectedCompanion = undefined;
+  // isSelected = true;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onCompanionSelected() {
+    this.selectedCompanion = 1;
+    console.log('in onCompanionSelected');
+  }
+
+  getBGColor() {
+    return this.selectedCompanion === 1 ? 'green' : 'gray';
+  }
 }
--- src/app/companions-list/companions-list.component.ts
+++ src/app/companions-list/companions-list.component.ts
@@ -5,12 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './companions-list.component.html',
   styleUrls: ['./companions-list.component.css']
 })
-export class CompanionsListComponent implements OnInit {
 
+export class CompanionsListComponent implements OnInit {
+  lookUpName = 'Ben';
+  selectedCompanion = undefined;
   companions = [
-    {name: 'Alex', rating: 8.4},
-    {name: 'Ben', rating: 8.2},
-    {name: 'Charlie', rating: 8.6}
+    {name: 'Alex', rating: 8.4, isFrequentlySelected: true, image: '../../assets/Alex.jpg'},
+    {name: 'Ben', rating: 8.2, isFrequentlySelected: false, image: '../../assets/Ben.jpg'},
+    {name: 'Cathy', rating: 8.6, isFrequentlySelected: true, image: '../../assets/Cathy.jpg'}
   ];
 
   constructor() { }
@@ -19,4 +21,13 @@ export class CompanionsListComponent implements OnInit {
     // console.log(this.companions[1]);
   }
 
+  onLookUpNameSelected(event: Event) {
+    this.lookUpName = (<HTMLInputElement>event.target).value;
+    console.log(this.lookUpName);
+  }
+
+  onCompanionSelected() {
+    this.selectedCompanion = 1;
+    console.log('in onCompanionSelected IN LIST');
+  }
 }
